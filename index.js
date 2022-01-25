479ad4490eec50fa103b931da2cb1124a4f266d6
Mayahi Adel <adel.mayahii@gmail.com>
2022-01-25 22:58:46 +0100

    Get restaurants

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import app from "./server.js";
 import mongodb from "mongodb";
 import dotenv from "dotenv";
+import RestaurantsDAO from "./dao/restaurantsDAO.js";
 dotenv.config();
 
 const MongoClient = mongodb.MongoClient;
@@ -14,6 +15,9 @@ MongoClient.connect(process.env.RESTREVIEWS_DB_URI, {
     console.error(err.stack);
     process.exit(1);
   })
-  .then(async (client) =>
-    app.listen(port, () => console.log(`listening on port ${port}`))
-  );
+  .then(async (client) => {
+    await RestaurantsDAO.injectDB(client);
+    app.listen(port, () => {
+      console.log(`listening on port ${port}`);
+    });
+  });
